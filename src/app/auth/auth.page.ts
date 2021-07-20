2154b22e9034e56394bfd93545744d8adec8a58e
gabyballester <gaby.ballester@gmail.com>
2021-07-20 13:15:43 +0200

    Receiving login/register data in the ts file ready for the api call

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -41,19 +41,18 @@ export class AuthPage implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
 
-  //   if (!form.valid) {
-  //     return;
-  //   }
-  //   const email = form.value.email;
-  //   const password = form.value.password;
-  //   console.log(email, password);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
 
-  //   if (this.isLogin) {
-  //     // Send a request to login servers
-  //   } else {
-  //     // Send a request to signup servers
-  //   }
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
 }
