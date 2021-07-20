ce56d03b78729c7dd4e4f60ceeee74c87debc5b0
gabyballester <gaby.ballester@gmail.com>
2021-07-20 17:11:15 +0200

    Using reactive forms module for the login form and validations

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -10,7 +10,30 @@ export class NewOfferPage implements OnInit {
   form: FormGroup;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
+  }
 
   onCreateOffer() {
     if (!this.form.valid) {
