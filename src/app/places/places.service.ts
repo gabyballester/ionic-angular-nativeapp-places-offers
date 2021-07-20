04b7f349b6dfe889681a87e96df78803dd61668f
gabyballester <gaby.ballester@gmail.com>
2021-07-20 18:31:43 +0200

    Creating a booking form and validation

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan mansion',
       'Amazing place in NYC',
       'https://th.bing.com/th/id/OIP.j1Jkxt2N-ha7_cX1ZYMMXwHaFT?w=258&h=184&c=7&o=5&dpr=1.05&pid=1.7',
-      149.99
+      149.99,
+      new Date(2021-7-1),
+      new Date(2021-7-15)
     ),
     new Place(
       'p2',
       "L'Amour TouJours",
       'Romantic place in Paris',
       'https://th.bing.com/th/id/OIP.PUGB36ymHAs8TbCVGwWiHAHaE8?w=216&h=180&c=7&o=5&dpr=1.05&pid=1.7',
-      189.99
+      189.99,
+      new Date(2021-7-1),
+      new Date(2021-7-15)
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://th.bing.com/th/id/OIP.sgOr8l5M1xkcNrlJ_XhWgwHaFj?w=260&h=195&c=7&o=5&dpr=1.05&pid=1.7',
-      99.99
+      99.99,
+      new Date(2021-7-1),
+      new Date(2021-7-15)
     ),
   ];
 
