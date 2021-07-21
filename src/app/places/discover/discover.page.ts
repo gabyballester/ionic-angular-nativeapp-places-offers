1fe606a7eac74419769ad4e0542baa66bf35982d
gabyballester <gaby.ballester@gmail.com>
2021-07-21 09:41:20 +0200

    Adjusting bookable places filtering by userid

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 import { SegmentChangeEventDetail} from '@ionic/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-discover',
@@ -13,18 +14,20 @@ import { Subscription } from 'rxjs';
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  relevantPlaces: Place[];
   private placesSub: Subscription;
 
   constructor(
     private placesService: PlacesService,
-    private menuCtrl: MenuController
+    private menuCtrl: MenuController,
+    private authService: AuthService
     ) {}
 
     ngOnInit() {
       this.placesSub = this.placesService.places.subscribe(places => {
         this.loadedPlaces = places;
-        // this.relevantPlaces = this.loadedPlaces;
-        this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+        this.relevantPlaces = this.loadedPlaces;
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
       });
     }
 
@@ -34,7 +37,15 @@ export class DiscoverPage implements OnInit {
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+    if (event.detail.value === 'all') {
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(
+        place => place.userId !== this.authService.userId
+      );
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    }
   }
 
   ngOnDestroy() {
